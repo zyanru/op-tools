7b69d11291d7054f24424a6cccf509157dfccec3
牛小栋 <niuxiaodong@niuxiaodongdeMacBook-Air.local>
2020-06-01 01:06:50 +0800

    add gecko.sh

--- README.md
+++ README.md
@@ -18,3 +18,12 @@ $ ./mycurl.sh --site www.baidu.com --thread 5 --count 10 --time 2
 2019-08-26 00:42:43 remote_ip: 39.156.66.18 http_code: 200
 
 ```
+
+## gecko.sh
+
+场景主要用于：`tail -n0 -f <logfile>`, 观察某个日志文件的输出，一定时间后自杀，然后分析输出的日志内容；如此反复，周期性的分析样本日志。
+
+这个脚本仅提供一个思路框架，task需要自己根据实际情况CODE
+
+
+
---
+++ gecko.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# Author ZhangYR<375092859@qq.com>
+
+# 定义需要tail分析的日志文件和一个tail后输出的临时文件
+RAW_LOGFILE=${1:=/var/log/nginx/access.log}
+TMP_LOGFILE=$(mktemp /tmp/gecko.XXXXX)
+## 
+## PROC_SLEEPTIME >= WORK_SLEEPTIME
+WORK_SLEEPTIME=${2:=60}
+PROC_SLEEPTIME=${3:=65}
+
+help()
+{
+    cat <<EOF
+Usage:
+    $0 <LogFile> <work time(sec)> <proc time(sec)>
+Example:
+    $0 --site www.baidu.com --thread 5 --count 50 --time 2
+ 
+EOF
+}
+
+task()
+{
+    # 额外的任务
+    # 可以分析$TMP_LOGFILE中的内容，然后各种计算、格式化输出
+    # Clear $TMP_LOGFILE
+    > $TMP_LOGFILE
+}
+
+reborn()
+{
+    # 自杀
+    # $! 代表最后一个执行的后台进程ID
+    echo "kill ProcessID: $!"
+    kill $!
+    # 自杀后，执行额外的任务
+    task
+}
+
+main()
+{
+    echo "My ProcessID: $$"
+    tail -n0 -f "${RAW_LOGFILE}" >> "$TMP_LOGFILE" &
+    trap reborn EXIT 0
+    # 持续 tail 某个日志文件多长时间
+    sleep $WORK_SLEEPTIME
+    # 当$WORK_SLEEPTIME时间完成后，给trap发送信号：exit 0
+    exit 0
+}
+
+if [ $# -lt 3 ]
+then
+    help
+    exit 1
+fi
+
+while true
+do
+    # 每隔多久（$PROC_SLEEPTIME），执行一次main
+    main &
+    sleep $PROC_SLEEPTIME
+done
+
